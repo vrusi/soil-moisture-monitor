54cc0e060fca9ec0ef0d1e6690ebee473396b610
vrusi <v.rusinkova97@gmail.com>
2020-07-18 01:08:56 +0200

    Add routes to sensors create, delete, index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var sensorsController = require('../controllers/sensorController');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Plants' });
 });
+
+router.post('/sensors', sensorsController.create);
+router.delete('/sensors/:id', sensorsController.delete);
+router.get('/sensors', sensorsController.index);
 
 module.exports = router;
