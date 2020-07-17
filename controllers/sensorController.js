5a28dfc20f497663c42a6a88a2c5064bf0dcd8cc
vrusi <v.rusinkova97@gmail.com>
2020-07-18 01:09:31 +0200

    Add create, delete, index to sensors controller

---
+++ controllers/sensorController.js
@@ -0,0 +1,39 @@
+var Sensor = require('../models').Sensor;
+
+exports.create = function (req, res) {
+    return Sensor
+        .create({
+            label: req.body.label,
+            airValue: req.body.airValue,
+            waterValue: req.body.waterValue,
+            version: req.body.version
+        })
+        .then((sensor) => res.status(201).send(sensor))
+        .catch((error) => res.status(400).send(error));
+}
+
+exports.delete = function (req, res) {
+    return Sensor
+        .findByPk(req.params.id)
+        .then(sensor => {
+            if (!sensor)
+                return res.status(204).send();
+
+            return sensor
+                .destroy()
+                .then(() => res.status(204).send())
+                .catch((error) => res.status(400).send(error));
+        })
+        .catch(() => res.status(204).send());
+}
+
+exports.index = function (req, res) {
+    return Sensor
+        .findAll({
+            order:
+                [['createdAt', 'DESC']]
+        })
+        .then((classrooms) => res.status(200).send(classrooms))
+        .catch((error) => { res.status(400).send(error); });
+}
+
