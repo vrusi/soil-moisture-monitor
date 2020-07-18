f24de16edd196440766f249af3b56899e61d7b2c
vrusi <v.rusinkova97@gmail.com>
2020-07-18 23:38:13 +0200

    Add interface for adding new sensors using axios

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import axios from 'axios';
 
 Vue.config.productionTip = false;
+window.axios = axios;
+axios.defaults.baseURL = 'http://localhost:3000';
 
 new Vue({
   router,
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form @submit.prevent="addSensor">
+      <input v-model="form.label" type="text" />
+      <input v-model="form.airValue" type="number" />
+      <input v-model="form.waterValue" type="number" />
+      <input v-model="form.version" type="text" />
+      <button type="submit">Add Sensor</button>
+    </form>
+    <ul>
+      <li v-for="sensor in sensors" :key="sensor.id">
+        {{sensor.id}}
+        {{sensor.label}}
+        {{sensor.airValue}}
+        {{sensor.waterValue}}
+        {{sensor.version}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      sensors: [],
+      form: {
+        label: "",
+        airValue: 0,
+        waterValue: 0,
+        version: ""
+      }
+    };
   },
+
+  async mounted() {
+    try {
+      const response = await window.axios.get("sensors");
+      this.sensors = response.data;
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  methods: {
+    async addSensor() {
+      try {
+        const response = await window.axios.post("/sensors", {
+          label: this.form.label,
+          airValue: this.form.airValue,
+          waterValue: this.form.waterValue,
+          version: this.form.version
+        });
+
+        this.sensors.push(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 };
 </script>
