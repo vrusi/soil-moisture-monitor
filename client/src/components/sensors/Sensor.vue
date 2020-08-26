c216ffb4ecbb6927eb2b344a094c5d9af3965b21
vrusi <v.rusinkova97@gmail.com>
2020-08-26 10:26:54 +0200

    Add placeholder content for sensors

--- client/src/components/sensors/Sensor.vue
+++ client/src/components/sensors/Sensor.vue
@@ -1,11 +1,40 @@
 <template>
-  <div>
-    <h1>Sensor</h1>
+  <div class="row">
+    <div class="card" style="width:50rem;">
+      <h1>{{ sensor.label }}</h1>
+      <div class="card-body">
+        <h5 class="card-title">Associated Plant: {{ sensor.plantID }}</h5>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Air Value: {{ sensor.airValue }}</li>
+        <li class="list-group-item">Water Value: {{ sensor.waterValue }}</li>
+        <li class="list-group-item">Version: {{ sensor.version }}</li>
+      </ul>
+      <div class="card-body">
+        <a :href="'/sensors/' + sensor.id" class="card-link">MORE</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      plants: [],
+    };
+  },
+  props: ["sensor"],
+};
 </script>
 
-<style>
+<style scoped>
+img {
+  width: 150px;
+}
+
+.card {
+  border: 1px solid lightgray;
+  border-radius: 2px;
+}
 </style>
--- client/src/components/sensors/Sensors.vue
+++ client/src/components/sensors/Sensors.vue
@@ -1,10 +1,37 @@
 <template>
-  <div>
-    <h1>Sensors</h1>
+  <div align="center" class="col">
+    <app-sensor v-for="sensor in sensors" :key="sensor.id" :sensor="sensor" style="margin: 30px"></app-sensor>
+    <hr />
+    <app-new-sensor></app-new-sensor>
   </div>
 </template>
 
 <script>
+import Sensor from "./Sensor.vue";
+import NewSensor from "./NewSensor.vue";
+
+export default {
+  data() {
+    return {
+      sensors: [],
+    };
+  },
+
+  components: {
+    appSensor: Sensor,
+    appNewSensor: NewSensor,
+  },
+
+  async mounted() {
+    try {
+      const responseSensors = await window.axios.get("sensors");
+      this.sensors = responseSensors.data;
+      console.log(responseSensors);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 </script>
 
 <style>
