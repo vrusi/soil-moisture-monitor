4443a9f8af0cfee99cb5b2d88965e5aabc82e830
vrusi <v.rusinkova97@gmail.com>
2020-08-29 01:28:38 +0200

    Add Vuetify

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import vSelect from "vue-select";
 import "vue-select/dist/vue-select.css";
 import store from "./store/store";
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 window.axios = axios;
@@ -21,5 +22,6 @@ export default {
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
