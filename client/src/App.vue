fc074d5fd85823cb54d714ed02fe19dc5dd254ac
vrusi <v.rusinkova97@gmail.com>
2020-09-04 23:29:48 +0200

    Remove unnecessary stuff

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,9 +2,7 @@
   <v-app>
     <v-main>
       <app-header></app-header>
-      <v-container fluid>
-        <router-view />
-      </v-container>
+      <router-view />
     </v-main>
   </v-app>
 </template>
@@ -33,25 +31,5 @@ export default {
 };
 </script>
 
-<style lang="scss">
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-} */
-
-/* #nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-} */
+<style>
 </style>
