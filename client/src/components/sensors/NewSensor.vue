9eebcc314c2f1e07ad0dfd129dcd518181a691d5
vrusi <v.rusinkova97@gmail.com>
2020-09-04 23:31:16 +0200

    Add plant selection

--- client/src/components/sensors/NewSensor.vue
+++ client/src/components/sensors/NewSensor.vue
@@ -2,6 +2,17 @@
   <div>
     <h1>New Sensor</h1>
     <form @submit.prevent="addSensor">
+      <v-select
+        :items="[...plants, 'None']"
+        :item-text="'name'"
+        :item-value="'id'"
+        :placeholder="'None'"
+        label="Attach to a plant"
+        v-model="plant"
+        dense
+        outlined
+        return-object
+      ></v-select>
       <input v-model="label" type="text" placeholder="label" />
       <input v-model.number="airValue" type="number" placeholder="air value" />
       <input v-model.number="waterValue" type="number" placeholder="water value" />
@@ -15,16 +26,23 @@
 export default {
   data() {
     return {
+      plant: null,
       label: "",
       airValue: null,
       waterValue: null,
       version: "",
     };
   },
+  computed: {
+    plants() {
+      return this.$store.getters.plants;
+    },
+  },
   methods: {
     async addSensor() {
       try {
         const response = await window.axios.post("/sensors", {
+          plantID: this.plant.id,
           label: this.label,
           airValue: this.airValue,
           waterValue: this.waterValue,
