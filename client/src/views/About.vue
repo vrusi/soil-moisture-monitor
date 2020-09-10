057a91ed3611931d3a492e976336e5c7cf556f85
vrusi <v.rusinkova97@gmail.com>
2020-09-10 02:14:29 +0200

    Update the about message

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-row>
+      <v-spacer></v-spacer>
+      <v-col>
+        <h1>Soil Moisture Monitor</h1>
+      </v-col>
+      <v-spacer></v-spacer>
+    </v-row>
+    <v-row>
+      <v-spacer></v-spacer>
+      <v-col>
+        <p>This is an app for monitoring the moisture of the soil your plants are potted in. This way you will always know when any of your plants need watering and you will be able to see how they are doing.</p>
+      </v-col>
+      <v-spacer></v-spacer>
+    </v-row>
+  </v-container>
 </template>
